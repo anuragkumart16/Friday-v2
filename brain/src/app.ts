163177511dfa0b27aa074a29032aa25cb4cfa35d
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2026-03-18 11:36:30 +0530

    added cors

--- brain/src/app.ts
+++ brain/src/app.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import cors from "cors";
 import { errorHandler } from "./middlewares/error.middleware";
 import { httpLogger } from "./middlewares/httpLogger.middleware";
 
@@ -12,6 +13,10 @@ import { httpLogger } from "./middlewares/httpLogger.middleware";
 const app = express();
 
 // app-level middleware config
+app.use(cors({
+    origin: process.env.DASHBOARD_URL || "http://localhost:3000",
+    credentials: true,
+}));
 app.use(httpLogger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
